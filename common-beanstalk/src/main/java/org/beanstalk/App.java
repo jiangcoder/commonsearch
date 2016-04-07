0b427efbdbaf718d68d2cb6c56ade758c260e13d
jiangcoder <jiangcoder@163.com>
2016-04-07 14:35:23 +0800

    add test

---
+++ common-beanstalk/src/main/java/org/beanstalk/App.java
@@ -0,0 +1,13 @@
+package org.beanstalk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
